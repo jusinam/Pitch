87ce64026c3babd7787f925bc4218ceb729e0d7b
DjCooGie <evansonchagwa01@gmail.com>
2020-01-12 19:13:39 +0300

    import the db module  and create User and Role models classes each with two columns that connects the app to the database

---
+++ app/models.py
@@ -0,0 +1,24 @@
+from . import db
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+
+
+    def __repr__(self):
+        return f'User {self.username}'
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+
+
+
+    def __repr__(self):
+        return f'User {self.name}'
