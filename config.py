64efcb5c5e205f47973964bac4702d91690565d4
DjCooGie <evansonchagwa01@gmail.com>
2020-01-14 00:44:38 +0300

    modify configuration for development purposes

--- config.py
+++ config.py
@@ -1,25 +1,17 @@
 import os
 
-class Config:
 
+
+
+class Config:
+    debug = True
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:hahaa@localhost/pitch'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    @staticmethod
-    def init_app(app):
-        pass
-
-
-class ProdConfig(Config):
-    pass
-
-
-class DevConfig(Config):
-    DEBUG = True
-
-config_options = {
-'development':DevConfig,
-'production':ProdConfig
-
-}
+    #  email configurations
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
