6f599cde31ab2b3f8766c66b8c713b436a9eaeaf
DjCooGie <evansonchagwa01@gmail.com>
2020-01-14 13:22:41 +0300

    deployment to heroku

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export MAIL_USERNAME ='evansonchagwa01@gmail.com'
+export MAIL_USERNAME='evansonchagwa01@gmail.com'
 export  MAIL_PASSWORD ='huhaa'
 export SECRET_KEY = 'hahaha'
 # SQLALCHEMY_DATABASE_URI='DATABASE_URL'
